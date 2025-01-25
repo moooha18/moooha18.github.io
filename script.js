9da64d3c9f46de21b3923a2d017a5c0826f97ab0
Mohammed Hadj Mahammed <24714851+moooha18@users.noreply.github.com>
2025-01-25 18:53:00 +0100

    fix c-sec

--- script.js
+++ script.js
@@ -24,33 +24,3 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-function showSuccessMessage(event) {
-    event.preventDefault(); // Prevent immediate submission
-    
-    // Get form data
-    const form = event.target;
-    const name = encodeURIComponent(form.name.value);
-    const email = encodeURIComponent(form.email.value);
-    const phone = encodeURIComponent(form.phone.value);
-    const subject = encodeURIComponent(form.subject.value);
-    const message = encodeURIComponent(form.message.value);
-    
-    // Create mailto link with form data
-    const mailtoLink = `mailto:hmmohamed0@gmail.com?subject=${subject}&body=Name: ${name}%0D%0AEmail: ${email}%0D%0APhone: ${phone}%0D%0AMessage: ${message}`;
-    
-    // Show success message
-    const successMessage = document.getElementById('successMessage');
-    successMessage.style.display = 'block';
-    
-    // Open email client
-    window.location.href = mailtoLink;
-    
-    // Hide message after 5 seconds
-    setTimeout(() => {
-      successMessage.style.display = 'none';
-    }, 5000);
-    
-    // Reset form
-    form.reset();
-  }
